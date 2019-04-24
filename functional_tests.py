fda08e59b63bda414773e2e86b6bc7cacb0f8b66
David Grant <davidgrant@gmail.com>
2019-04-23 23:19:10 -0700

    more basic functionality

--- functional_tests.py
+++ functional_tests.py
@@ -1,27 +1,58 @@
 #!/usr/bin/env python3
+import time
 
 from selenium import webdriver
 import unittest
 
-class NewVisitorTest(unittest.TestCase):  
+from selenium.webdriver.common.keys import Keys
 
-    def setUp(self):  
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def tearDown(self):  
+    def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  
+    def test_can_create_a_team_and_add_players(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
-        # She notices the page title and header mention to-do lists
-        self.assertIn('Team Manager', self.browser.title)  
-        self.fail('Finish the test!')  
+        # She notices the page title and header mention Team Manager
+        self.assertIn('Team Manager', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Team Manager', header_text)
+
+        # She is invited to create a team straight away
+        inputbox = self.browser.find_element_by_id('id_new_team')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a team name'
+        )
+
+        # She types "Grey Wolves" into a text box)
+        inputbox.send_keys('Grey Wolves')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "Grey Wolves" as an item in a list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'Grey Wolves' for row in rows),
+            'New team did not appear in the output rows'
+        )
+        self.assertTrue(len(rows) == 1)
+
+        # Now there is a place for her to click and add new players to the team
+        self.fail('Finish the test!')
 
         # She is invited to create a new team straight away
 #        [...rest of comments as before]
 
-if __name__ == '__main__':  
-    unittest.main(warnings='ignore')  
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
--- team/tests.py
+++ team/tests.py
@@ -1,18 +1,8 @@
-from django.http import HttpRequest
-from django.urls import resolve
 from django.test import TestCase
-from team.views import home_page
+
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found_view = resolve('/')
-        self.assertEqual(found_view.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>Team Manager</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
