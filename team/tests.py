9118bc081d3eeeb224d91cccf2b888b02a90a7c5
David Grant <davidgrant@gmail.com>
2020-02-23 23:30:59 -0800

    progress

--- team/tests.py
+++ team/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from team.models import Team
 
 class HomePageTest(TestCase):
 
@@ -11,3 +12,21 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'team_name': 'Grey Wolves'})
         self.assertIn('Grey Wolves', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class TeamModelTest(TestCase):
+
+    def test_saving_and_retrieving_team(self):
+        first_team = Team()
+        first_team.name = "my team"
+        first_team.save()
+
+        second_team = Team()
+        second_team.name = "other team"
+        second_team.save()
+
+        self.assertEqual(2, Team.objects.count())
+
+        teams = Team.objects.all()
+        self.assertEqual(first_team.name, teams[0].name)
+        self.assertEqual(second_team.name, teams[1].name)
