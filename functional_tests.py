3ca265d2826988fa988406902b903218498c086a
David Grant <davidgrant@gmail.com>
2020-02-23 16:33:18 -0800

    progress

--- functional_tests.py
+++ functional_tests.py
@@ -16,6 +16,12 @@ class NewVisitorTest(unittest.TestCase):
         pass
         # self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_create_a_team_and_add_players(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -40,14 +46,25 @@ class NewVisitorTest(unittest.TestCase):
         # "Grey Wolves" as an item in a list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table("Grey Wolves")
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == 'Grey Wolves' for row in rows),
-            'New team did not appear in the output rows'
+        # There is still a text box allowing to enter another team. She enters another team.
+        inputbox = self.browser.find_element_by_id('id_new_team')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a team name'
         )
-        self.assertTrue(len(rows) == 1)
+
+        # She types "Black Sox" into a text box)
+        inputbox.send_keys('Black Sox')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "Black Sox" as an item in a list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('Black Sox')
+        self.check_for_row_in_list_table('Grey Wolves')
 
         # Now there is a place for her to click and add new players to the team
         self.fail('Finish the test!')
--- team/views.py
+++ team/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    if request.method == 'POST':
-        return HttpResponse(request.POST['team_name'])
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_teamname': request.POST.get('team_name', ''),
+    })
